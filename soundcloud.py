d7fb644d3a75222f47cb09ede0f05de6652c12ac
David <david@hermes.(none)>
2011-11-05 17:49:29 -0700

    Getting song title too

--- soundcloud.py
+++ soundcloud.py
@@ -2,21 +2,23 @@ from optparse import OptionParser
 import re
 from browser_clone import url_open
 
-def get_stream_token_uid(page):
+def get_stream_url(page):
     """ returns stream token and uid as tuple """
-    match = re.search('"uid":"([\w\d]+?)".*?stream_token=([\w\d]+)', page)
+    match = re.search('http://media.soundcloud.com/stream/.*?(?=\")', page)
     if match:
-        uid = match.group(1)
-        stream_token = match.group(2)
-        return (uid, stream_token)
+        return match.group(0)
 
-def download(uid, token):
+def get_song_title(page):
+    match = re.search('(?<=\"title\":\").*?(?=\")', page)
+    if match:
+        return match.group(0)
+
+def download(stream_url, song_title):
     """ given url with token and uid, download file to mp3 """
+    response = url_open(stream_url)
+    song_file = song_title + '.mp3'
 
-    url = "http://media.soundcloud.com/stream/%s?stream_token=%s" % (uid, token)
-    response = url_open(url)
-
-    f = open('test.mp3', 'w')
+    f = open(song_file, 'w')
     f.write(response.read())
 
 if __name__ == '__main__':
@@ -24,14 +26,18 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-u", "--url")
     (options, args) = parser.parse_args()
+    if not options.url:
+        print "Need to give a URL silly"
+    else:
+        # open up initial page (argument) to get stream token and uid
+        response = url_open(options.url)
+        html = response.read()
+        stream_url = get_stream_url(html)
+        song_title = get_song_title(html)
+        print song_title
 
-    # open up initial page (argument) to get stream token and uid
-    response = url_open(options.url)
-    html = response.read()
-    (uid, token) = get_stream_token_uid(html)
+        # the browser does this...so we will too
+        #response = url_open('http://media.soundcloud.com/crossdomain.xml')
 
-    # the browser does this...so we will too
-    response = url_open('http://media.soundcloud.com/crossdomain.xml')
-
-    # compose a url with uid and token and request the mpeg
-    download(uid, token)
+        # compose a url with uid and token and request the mpeg
+        #download(stream_url)
